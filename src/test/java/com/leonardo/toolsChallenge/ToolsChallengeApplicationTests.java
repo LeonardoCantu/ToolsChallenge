87637dd54ef709c3ea81ab5781da21ddd6c30ae0
LeonardoCantu <leocantu.gh@gmail.com>
2022-10-03 15:11:31 -0300

    first commit

---
+++ src/test/java/com/leonardo/toolsChallenge/ToolsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.toolsChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
